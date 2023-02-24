2e189444ee77c2df5c726b3369de5e0441735b19
pushpavalli <pushpavalli551@gmail.com>
2023-02-23 21:20:31 -0500

    ecommerce pages commit

---
+++ onlinefood/src/app/services/food.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { sample_foods } from 'src/data';
+import { Food } from '../shared/models/Food';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FoodService {
+
+  constructor() { }
+
+  getAll():Food[]{
+    return sample_foods;
+  }
+
+  getAllFoodsBySearchTerm(searchTerm:string){
+    return this.getAll().filter(food => food.name.toLowerCase().includes(searchTerm.toLowerCase()))
+  }
+}
